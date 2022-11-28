1fb6fc8f19fa8e173403496100f4ef518edf6cd1
danikaharu <dani.kaharu17@gmail.com>
2022-11-28 11:27:25 +0800

    adjust widht logo

--- resources/views/partials/user/navbar.blade.php
+++ resources/views/partials/user/navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
 
         <a class="navbar-brand" href="#"><img src="{{ asset('template_user/assets/img/logo/logo.svg') }} "
-                alt="logo" width="300"></a>
+                alt="logo" width="250"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
