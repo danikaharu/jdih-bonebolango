bca090ac398e8e0006db683905faa9846e9bcf29
danikaharu <dani.kaharu17@gmail.com>
2022-11-28 11:30:40 +0800

    adjust limit pagination number

--- resources/views/vendor/pagination/custom.blade.php
+++ resources/views/vendor/pagination/custom.blade.php
@@ -1,6 +1,7 @@
 @if ($paginator->hasPages())
     <nav aria-label="Page navigation example">
         <ul class="pagination custom-pagination">
+            {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
                 <li class="page-item disabled">
                     <a class="page-link" href="#" tabindex="-1"><img
@@ -12,7 +13,45 @@
                 </li>
             @endif
 
-            @foreach ($elements as $element)
+            <?php
+            $start = $paginator->currentPage() - 2; // show 3 pagination links before current
+            $end = $paginator->currentPage() + 2; // show 3 pagination links after current
+            if ($start < 1) {
+                $start = 1; // reset start to 1
+                $end += 1;
+            }
+            if ($end >= $paginator->lastPage()) {
+                $end = $paginator->lastPage();
+            } // reset end to last page
+            ?>
+
+            @if ($start > 1)
+                <li class="page-item">
+                    <a class="page-link" href="{{ $paginator->url(1) }}">{{ 1 }}</a>
+                </li>
+                @if ($paginator->currentPage() != 4)
+                    {{-- "Three Dots" Separator --}}
+                    <li class="page-item disabled" aria-disabled="true"><span class="page-link">...</span></li>
+                @endif
+            @endif
+            @for ($i = $start; $i <= $end; $i++)
+                <li class="page-item {{ $paginator->currentPage() == $i ? ' active' : '' }}">
+                    <a class="page-link" href="{{ $paginator->url($i) }}">{{ $i }}</a>
+                </li>
+            @endfor
+            @if ($end < $paginator->lastPage())
+                @if ($paginator->currentPage() + 3 != $paginator->lastPage())
+                    {{-- "Three Dots" Separator --}}
+                    <li class="page-item disabled" aria-disabled="true"><span class="page-link">...</span></li>
+                @endif
+                <li class="page-item">
+                    <a class="page-link"
+                        href="{{ $paginator->url($paginator->lastPage()) }}">{{ $paginator->lastPage() }}</a>
+                </li>
+            @endif
+
+
+            {{-- @foreach ($elements as $element)
                 @if (is_string($element))
                     <li class="page-item disabled">{{ $element }}</li>
                 @endif
@@ -29,8 +68,9 @@
                         @endif
                     @endforeach
                 @endif
-            @endforeach
+            @endforeach --}}
 
+            {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
                 <li class="page-item">
                     <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">
