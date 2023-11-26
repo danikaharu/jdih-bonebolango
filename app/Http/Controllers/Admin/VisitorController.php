458ffc41158dec9f8fc33407397e6870a0c8ba46
danikaharu <dani.kaharu17@gmail.com>
2023-11-26 16:20:19 +0800

    add statistic visitor

--- app/Http/Controllers/Admin/VisitorController.php
+++ app/Http/Controllers/Admin/VisitorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use AkkiIo\LaravelGoogleAnalytics\Facades\LaravelGoogleAnalytics;
 use AkkiIo\LaravelGoogleAnalytics\Period;
+use Illuminate\Support\Carbon;
 use Google\Analytics\Data\V1beta\Filter\StringFilter\MatchType;
 
 class VisitorController extends Controller
@@ -15,9 +16,10 @@ class VisitorController extends Controller
         $mostViewsByPage = LaravelGoogleAnalytics::getMostViewsByPage(Period::days(30), $count = 10);
         $totalUsersPerMonth = LaravelGoogleAnalytics::getTotalUsers(Period::months(1));
         $totalUsersPerDay = LaravelGoogleAnalytics::getTotalUsers(Period::days(1));
+        $totalUsers = LaravelGoogleAnalytics::getTotalUsers(Period::create(Carbon::createFromFormat('Y-m-d', '2021-04-28'), Carbon::now()));
         $popularProduct = $this->popularProduct();
 
-        return view('admin.visitor.index', compact('mostViewsByPage', 'totalUsersPerMonth', 'totalUsersPerDay', 'popularProduct'));
+        return view('admin.visitor.index', compact('mostViewsByPage', 'totalUsersPerMonth', 'totalUsersPerDay', 'totalUsers', 'popularProduct'));
     }
 
     private function popularProduct()
--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -7,6 +7,9 @@ use App\Models\Profile;
 use App\Models\Survey;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Carbon;
+use AkkiIo\LaravelGoogleAnalytics\Facades\LaravelGoogleAnalytics;
+use AkkiIo\LaravelGoogleAnalytics\Period;
 
 class ViewServiceProvider extends ServiceProvider
 {
@@ -31,6 +34,22 @@ class ViewServiceProvider extends ServiceProvider
 
         View::composer('user.home.include.rating', function ($view) {
             $view->with('ratings', number_format(Survey::avg('rating'), 2));
+        });
+
+        View::composer(['partials.user.footer'], function ($view) {
+            $startDate = Carbon::createFromFormat('Y-m-d', '2021-04-01');
+            $endDate = Carbon::now();
+
+            $totalVisitor = LaravelGoogleAnalytics::getTotalUsers(Period::create($startDate, $endDate));
+            $totalVisitorMonth = LaravelGoogleAnalytics::getTotalUsers(Period::months(1));
+            $totalVisitorToday = LaravelGoogleAnalytics::getTotalUsers(Period::days(1));
+            return $view->with(
+                [
+                    'totalVisitor' => $totalVisitor,
+                    'totalVisitorMonth' => $totalVisitorMonth,
+                    'totalVisitorToday' => $totalVisitorToday,
+                ],
+            );
         });
     }
 }
